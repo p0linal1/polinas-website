580e70031a0f2ab2fbae99f165dbaddf09f9dcb2
p0linal <polinalouki@gmail.com>
2025-12-03 22:15:30 -0800

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+// import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return (
+        <div className = "min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/*Theme toggle button */}    
+        <ThemeToggle />
+        {/*Background effect? */}
+
+
+        {/*Navbar */}
+
+
+        {/*Main content */}
+
+        
+        {/*Footer */}
+
+        </div>
+    );
+}
